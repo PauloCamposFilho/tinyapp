0e1d7164f20b187c6a6d4d64f5a8195dd768cc8f
Paulo Campos Filho <pezaum@gmail.com>
2023-06-14 17:38:54 -0700

    Added new permissions/restrictions.

--- express_server.js
+++ express_server.js
@@ -6,8 +6,22 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userId: "a2b3c4"
+   },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userId: "a2b3c4"
+   },
+   "v9smk3": {
+    longURL: "http://www.yahoo.com",
+    userId: "bv12cd"
+   },
+   "vh8sdx": {
+    longURL: "http://www.example.com",
+    userId: "bv12cd"
+   }
 };
 
 const users = {
@@ -24,6 +38,36 @@ const users = {
 };
 
 // Internal Functions for the Server
+
+const shortURLCodeExists = (shortURL) => {  
+  return (Object.prototype.hasOwnProperty.call(urlDatabase, shortURL));
+};
+
+const getUrlsByUser = (user) => {
+  const returnObj = {};
+  const userId = user.id;  
+  console.log("passed userId:", userId)
+  if (userId) {
+    for (let shortURLCode in urlDatabase) {      
+      if (urlDatabase[shortURLCode].userId === userId) {
+        returnObj[shortURLCode] = urlDatabase[shortURLCode].longURL;
+      }
+    }
+  }  
+  return returnObj;
+};
+
+const userOwnsUrl = (user, shortURLCode) => {
+  const userId = user.id;  
+  console.log("userId:", userId);
+  console.log("shortCode:", shortURLCode);
+  if (Array.prototype.hasOwnProperty.call(urlDatabase, shortURLCode)) {
+    if (urlDatabase[shortURLCode].userId === userId) {
+      return true;
+    }    
+  }
+  return false;
+};
 
 const generateRandomString = (length) => {
   const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -45,7 +89,6 @@ const parseLongURL = (longURL) => {
 };
 
 const isUserLoggedIn = (cookies) => {
-  //if (cookies["user_id"] && users.hasOwnProperty(cookies["user_id"])) {
   if (cookies["user_id"] && Object.prototype.hasOwnProperty.call(users, cookies["user_id"])) {
     return true;
   }
@@ -97,11 +140,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: {} };
+  const templateVars = { user: {} };
   if (!isUserLoggedIn(req.cookies)) {
     return res.redirect("/login");
   }
   templateVars.user = users[req.cookies["user_id"]];
+  templateVars.urls = getUrlsByUser(templateVars.user);
   console.log(templateVars);
   res.render("urls_index", templateVars);
 });
@@ -120,15 +164,23 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     user: {}
-  };
+  };  
   if (!isUserLoggedIn(req.cookies)) {
     return res.redirect("/login");
   }
-  if (req.params && req.params.id) {
-    templateVars.id = req.params.id;
-    templateVars.longURL = urlDatabase[templateVars.id];
-    templateVars.user = users[req.cookies["user_id"]];
+  templateVars.user = users[req.cookies["user_id"]];
+  
+  if (!shortURLCodeExists(req.params.id)) {
+    templateVars.message = `Invalid ShortCode`;
+    return res.status(400).render("showMessage", templateVars);  }
+
+  if (!userOwnsUrl(templateVars.user, req.params.id)) {
+    templateVars.message = `Access Denied. You do not have access to view/edit ${req.params.id}`
+    return res.status(401).render("showMessage", templateVars);
   }
+  templateVars.id = req.params.id;
+  templateVars.longURL = urlDatabase[templateVars.id].longURL;  
+  
   res.render("url_shows", templateVars);
 });
 
@@ -157,17 +209,17 @@ app.get("/u/:id", (req, res) => {
   const templateVars = {
     user: {}
   };
-  // if (!isUserLoggedIn(req.cookies)) {
-  //   templateVars.message = "Cannot redirect. User is not logged in.";
-  //   return res.render("showMessage", templateVars);
-  // }
+
   if (Object.prototype.hasOwnProperty.call(urlDatabase, req.params.id)) {
-    console.log(`Sending them to: ${urlDatabase[req.params.id]}`);
-    res.redirect(urlDatabase[req.params.id]);
+    console.log(`Sending them to: ${urlDatabase[req.params.id].longURL}`);
+    res.redirect(urlDatabase[req.params.id].longURL);
   } else {
     console.log(`Sending them ...nowhere hopefully.`);
-    res.statusCode = 404;
-    res.send(`Invalid code: ${req.params.id}`);
+    if (isUserLoggedIn(req.cookies)) {
+      templateVars.user = users[req.cookies["user_id"]];
+    }
+    templateVars.message = `Invalid shortCode: ${req.params.id}`;
+    res.status(404).render("showMessage", templateVars)    
   }
 });
 
@@ -181,12 +233,18 @@ app.post("/urls", (req, res) => {
     templateVars.message = "Cannot create short URL. User is not logged in.";
     return res.render("showMessage", templateVars);
   }
-  // templateVars.user = users[req.cookies["user_id"]];
-  if (req.body.longURL) {
-    const shortURL = generateRandomString(6);
-    urlDatabase[shortURL] = parseLongURL(req.body.longURL);
-    res.redirect(`/urls/${shortURL}`);
+  templateVars.user = users[req.cookies["user_id"]];
+  if (!req.body.longURL) {
+    templateVars.message = "The URL cannot be empty.";
+    return res.status(400).render("showMessage", templateVars);
   }
+  const shortURL = generateRandomString(6);
+  const newLink = {
+    longURL: parseLongURL(req.body.longURL),
+    userId: templateVars.user.id
+  };    
+  urlDatabase[shortURL] = newLink;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -194,8 +252,21 @@ app.post("/urls/:id/delete", (req, res) => {
     user: {}
   };
   if (!isUserLoggedIn(req.cookies)) {
-    templateVars.message = "Cannot delete URL. User is not logged in.";
+    templateVars.message = `Access Denied. You do not have access to delete ${req.params.id}`
     return res.render("showMessage", templateVars);
+  }
+  templateVars.user = users[req.cookies["user_id"]];
+  console.log(":id/delete route, id:", req.params.id);  
+  if (!shortURLCodeExists(req.params.id)) { // another error
+    console.log("I shouldnt be here...");
+    templateVars.message = `Invalid ShortCode`;
+    return res.status(400).render("showMessage", templateVars);
+  }
+  if (!userOwnsUrl(templateVars.user, req.params.id)) { // different error
+    templateVars.message = "Access denied."
+    console.log("Access was denid.");
+    console.log(templateVars);
+    return res.status(401).render("showMessage", templateVars);
   }
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
@@ -209,8 +280,17 @@ app.post("/urls/:id/update", (req, res) => {
     templateVars.message = "Cannot update URL. User is not logged in.";
     return res.render("showMessage", templateVars);
   }
+  templateVars.user = users[req.cookies["user_id"]];
+  if (!shortURLCodeExists(req.params.id)) { // another error
+    templateVars.message = `Invalid ShortCode`;
+    return res.status(400).render("showMessage", templateVars);
+  }
+  if (!userOwnsUrl(templateVars.user, req.params.id)) { // different error
+    templateVars.message = `Access Denied. You do not have access to delete ${req.params.id}`
+    return res.render("showMessage", templateVars);    
+  }
   if (req.body.longURL) {
-    urlDatabase[req.params.id] = parseLongURL(req.body.longURL);
+    urlDatabase[req.params.id].longURL = parseLongURL(req.body.longURL);
   }
   res.redirect("/");
 });
